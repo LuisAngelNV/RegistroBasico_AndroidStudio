b87e8d4df815bec145e4d6abcda7dea55b54c48e
LuisAngelNV <deanda.1998@gmail.com>
2023-10-18 20:02:46 -0500

    Agregar barra de navegación

--- app/src/main/java/com/luisangelnv/socialgame/activities/PageHome.java
+++ app/src/main/java/com/luisangelnv/socialgame/activities/PageHome.java
@@ -2,13 +2,18 @@ package com.luisangelnv.socialgame.activities;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.MenuItem;
 import android.view.View;
 
 import androidx.core.view.WindowCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -16,39 +21,42 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.luisangelnv.socialgame.R;
 import com.luisangelnv.socialgame.databinding.ActivityPageHomeBinding;
+import com.luisangelnv.socialgame.fragments.Chats;
+import com.luisangelnv.socialgame.fragments.Filters;
+import com.luisangelnv.socialgame.fragments.Home;
+import com.luisangelnv.socialgame.fragments.Profile;
 
 public class PageHome extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityPageHomeBinding binding;
+    BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityPageHomeBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_page_home);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
-            }
-        });
+        setContentView(R.layout.activity_page_home);
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        openFragment(new Home());
     }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_page_home);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        ((FragmentTransaction) transaction).addToBackStack(null);
+        transaction.commit();
     }
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    if (item.getItemId() == R.id.ItemHome){
+                        openFragment(new Home());
+                    } else if (item.getItemId() == R.id.ItemChat) {
+                        openFragment(new Chats());
+                    } else if (item.getItemId()== R.id.ItemFilters) {
+                        openFragment(new Filters());
+                    } else if (item.getItemId() == R.id.ItemProfile)  {
+                        openFragment(new Profile());
+                    }
+                return true;
+           }
+    };
 }
