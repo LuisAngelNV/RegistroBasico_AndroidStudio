abdbf149de77b52335a37797d33fcd6218b0fdf2
LuisAngelNV <deanda.1998@gmail.com>
2023-10-18 23:20:42 -0500

    Diseño para agregar productos

---
+++ app/src/main/java/com/luisangelnv/socialgame/activities/Post.java
@@ -0,0 +1,14 @@
+package com.luisangelnv.socialgame.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.luisangelnv.socialgame.R;
+
+public class Post extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+    }
+}
--- app/src/main/java/com/luisangelnv/socialgame/fragments/Home.java
+++ app/src/main/java/com/luisangelnv/socialgame/fragments/Home.java
@@ -1,5 +1,6 @@
 package com.luisangelnv.socialgame.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,46 +9,35 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.luisangelnv.socialgame.R;
+import com.luisangelnv.socialgame.activities.Post;
 
 public class Home extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Home() {
+    View mView;
+    FloatingActionButton mFAB;
+        public Home() {
         // Required empty public constructor
     }
 
-    // TODO: Rename and change types and number of parameters
-    public static Home newInstance(String param1, String param2) {
-        Home fragment = new Home();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        mView = inflater.inflate(R.layout.fragment_home, container, false);
+        mFAB = mView.findViewById(R.id.FAB);
+        mFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToPost();
+            }
+        });
+        return mView;
+    }
+
+    private void goToPost() {
+            Intent intent = new Intent(getContext(), Post.class);
+            startActivity(intent);
     }
 }
