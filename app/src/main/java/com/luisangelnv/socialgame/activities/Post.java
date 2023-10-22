daabdfe817a8c72f1e50c49863acdf0981213884
LuisAngelNV <deanda.1998@gmail.com>
2023-10-21 21:26:43 -0500

    Subir imagen a Firebase 02 / 02 y tomar de galeria 2 imagenes

--- app/src/main/java/com/luisangelnv/socialgame/activities/Post.java
+++ app/src/main/java/com/luisangelnv/socialgame/activities/Post.java
@@ -4,20 +4,29 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.storage.UploadTask;
 import com.luisangelnv.socialgame.R;
 import com.luisangelnv.socialgame.models.Publicaciones;
 import com.luisangelnv.socialgame.providers.AuthProviders;
@@ -28,17 +37,23 @@ import com.luisangelnv.socialgame.utils.FileUtil;
 import java.io.File;
 import java.util.Objects;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+import dmax.dialog.SpotsDialog;
+
 public class Post extends AppCompatActivity {
 
     ImageProvider mImageProvider;
-    ImageView mImageViewPostOne, mImageViewPC, mImageViewPS4, mImageViewXbox, mImageViewNintendo ;
-    File mImageFile;
+    ImageView mImageViewPostOne, mImageViewPosDos, mImageViewPC, mImageViewPS4, mImageViewXbox, mImageViewNintendo ;
+    File mImageFile, mImageFile2;
     Button mButtonPost;
     TextInputEditText mTextInputTitle, mTextInputDescription;
     TextView mTextViewCategory;
     String mCategory = "", mTitle = "", mDescription = "";
     PostProvider mPostProvider;
     AuthProviders mAuthProvider;
+    private final int GALLERY_REQUEST_CODE =1 ,  GALLERY_REQUEST_CODE_2 = 2;
+    AlertDialog mDialog;
+    CircleImageView mCircleImageBack;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -47,6 +62,7 @@ public class Post extends AppCompatActivity {
         setContentView(R.layout.activity_post);
         mImageProvider = new ImageProvider();
         mImageViewPostOne = findViewById(R.id.ImageViewPost1);
+        mImageViewPosDos = findViewById(R.id.ImageViewPost2);
         mButtonPost = findViewById(R.id.btnPost);
         mTextInputTitle = findViewById(R.id.TxtInputVideoGame);
         mTextInputDescription = findViewById(R.id.TxtInputDescription);
@@ -55,8 +71,11 @@ public class Post extends AppCompatActivity {
         mImageViewXbox = findViewById(R.id.ImageViewXbox);
         mImageViewNintendo = findViewById(R.id.ImageViewNintendo);
         mTextViewCategory = findViewById(R.id.TextViewCategory);
+        mCircleImageBack = findViewById(R.id.circleImageBack);
+        mCircleImageBack.setOnClickListener(v -> finish());
         mButtonPost.setOnClickListener(v -> ClicPost());
-        mImageViewPostOne.setOnClickListener(view -> OpenGallery());
+        mImageViewPostOne.setOnClickListener(view -> OpenGallery(GALLERY_REQUEST_CODE));
+        mImageViewPosDos.setOnClickListener(view -> OpenGallery(GALLERY_REQUEST_CODE_2));
         mImageViewPC.setOnClickListener(v -> {
             mCategory= "PC";
             mTextViewCategory.setText(mCategory);
@@ -75,6 +94,10 @@ public class Post extends AppCompatActivity {
         });
         mPostProvider = new PostProvider();
         mAuthProvider = new AuthProviders();
+        mDialog = new SpotsDialog.Builder()
+                .setContext(this)
+                .setMessage("Espere un momento, por favor...")
+                .setCancelable(false).build();
     }
 
 
@@ -95,23 +118,39 @@ public class Post extends AppCompatActivity {
     }
 
     private void saveImage() {
+        mDialog.show();
         mImageProvider.save(Post.this, mImageFile).addOnCompleteListener(task -> {
             if (task.isSuccessful()) {
                 mImageProvider.getStorage().getDownloadUrl().addOnSuccessListener(uri -> {
                     String url = uri.toString();
-                    Publicaciones post = new Publicaciones();
-                    post.setImage1(url);
-                    post.setTitle(mTitle);
-                    post.setDescription(mDescription);
-                    post.setCategory(mCategory);
-                    post.setIdUser(mAuthProvider.getUid());
-                    mPostProvider.save(post).addOnCompleteListener(taskSave -> {
-                        if (taskSave.isSuccessful()){
-                            Toast.makeText(Post.this, "Muy bien, proceso finalizado", Toast.LENGTH_SHORT).show();
-                        }else{
-                            Toast.makeText(Post.this, "Algo no ha funcionado bien", Toast.LENGTH_SHORT).show();
+                    mImageProvider.save(Post.this, mImageFile2).addOnCompleteListener(taskImage2 -> {
+                        if(taskImage2.isSuccessful()){
+                            mImageProvider.getStorage().getDownloadUrl().addOnSuccessListener(uri2 -> {
+                                String url2 = uri2.toString();
+                                Publicaciones post = new Publicaciones();
+                                post.setImage1(url);
+                                post.setImage2(url2);
+                                post.setTitle(mTitle);
+                                post.setDescription(mDescription);
+                                post.setCategory(mCategory);
+                                post.setIdUser(mAuthProvider.getUid());
+                                mPostProvider.save(post).addOnCompleteListener(taskSave -> {
+                                    mDialog.dismiss();
+                                    if (taskSave.isSuccessful()){
+                                        ClearForm();
+                                        Toast.makeText(Post.this, "Muy bien, proceso finalizado", Toast.LENGTH_SHORT).show();
+                                    }else{
+                                        mDialog.dismiss();
+                                        Toast.makeText(Post.this, "Algo no ha funcionado bien", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            });
+                        }else {
+                            mDialog.dismiss();
+                            Toast.makeText(this, "Oh no...\n\rSe ha producido un error al momento de guaradr tu imagem.", Toast.LENGTH_LONG).show();
                         }
                     });
+
                 });
                 Log.d( "Campo", "Carga de imagen con exito al sistema");
             }else{
@@ -121,24 +160,50 @@ public class Post extends AppCompatActivity {
         });
     }
 
-    private void OpenGallery() {
-        Intent GalleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
-        GalleryIntent.setType("image/*");
-        galleryLauncher.launch(GalleryIntent);
-        //startActivityForResult(GalleryIntent, Gallery_Request_Code);
-
+    private void ClearForm() {
+        mTextInputTitle.setText("");
+        mTextInputDescription.setText("");
+        mTextViewCategory.setText("");
+        mImageViewPostOne.setImageResource(R.drawable.upload_image);
+        mImageViewPosDos.setImageResource(R.drawable.upload_image);
+        mTitle = "";
+        mDescription = "";
+        mCategory = "";
+        mImageFile = null;
+        mImageFile2 = null;
     }
 
-    /*
+    /** @noinspection deprecation*/
+    private void OpenGallery(int requestCode) {
+        Intent GalleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        GalleryIntent.setType("image/*");
+        //galleryLauncher.launch(GalleryIntent);
+        startActivityForResult(GalleryIntent, requestCode);
+    }
+
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == Gallery_Request_Code && resultCode == RESULT_OK) {
-
+        if (requestCode == GALLERY_REQUEST_CODE && resultCode == RESULT_OK) {
+            try {
+                mImageFile = FileUtil.from(Post.this, data.getData());
+                mImageViewPostOne.setImageBitmap(BitmapFactory.decodeFile(mImageFile.getAbsolutePath()));
+            } catch (Exception e) {
+                Toast.makeText(Post.this, "Se ha tenido un error con: " + e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+        if (requestCode == GALLERY_REQUEST_CODE_2 && resultCode == RESULT_OK) {
+            try {
+                mImageFile2 = FileUtil.from(Post.this, data.getData());
+                mImageViewPosDos.setImageBitmap(BitmapFactory.decodeFile(mImageFile2.getAbsolutePath()));
+            } catch (Exception e) {
+                Toast.makeText(Post.this, "Se ha tenido un error con: " + e.getMessage(), Toast.LENGTH_LONG).show();
+            }
         }
     }
-    */
-    ActivityResultLauncher<Intent> galleryLauncher = registerForActivityResult(
+
+    /*ActivityResultLauncher<Intent> galleryLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             new ActivityResultCallback<ActivityResult>() {
                 @Override
@@ -154,5 +219,5 @@ public class Post extends AppCompatActivity {
                     }
                 }
             }
-    );
+    );*/
 }
--- app/src/main/java/com/luisangelnv/socialgame/providers/ImageProvider.java
+++ app/src/main/java/com/luisangelnv/socialgame/providers/ImageProvider.java
@@ -20,7 +20,7 @@ public class ImageProvider {
 
     public UploadTask save(Context context, File file){
         byte[] imageByte = CompressorBitmapImage.getImage(context, file.getPath(), 500, 500);
-        StorageReference storage = mStorage.child(new Date()+ ".jpg");
+        StorageReference storage = FirebaseStorage.getInstance().getReference().child(new Date()+ ".jpg");
         mStorage = storage;
         return storage.putBytes(imageByte);
     }
